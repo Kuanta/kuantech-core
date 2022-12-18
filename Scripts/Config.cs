6ed4c6f8840f5b4481216375099ca49dc4c54b70
doruk-veloxia <erdemhan93@gmail.com>
2022-12-19 00:23:47 +0300

    Adds missing config script

---
+++ Scripts/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kuantech
+{
+    [Serializable]
+    public static class Config
+    {
+        public const float LEVEL_TO_HEALTH_FACTOR = 1f;
+        public const float LEVEL_TO_ENERGY_FACTOR = 1f;
+
+        public const float DEFAULT_LEVEL_TO_STAT_FACTOR = 1f;
+
+        public const float LEVEL_FORMULA_X = 0.3f;
+        public const float LEVEL_FORMULA_Y = 2;
+
+        public const float MAX_ENCUMBRANCE = 10f;
+    }
+}
