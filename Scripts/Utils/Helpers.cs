b404ed2558707b5ba50f76b787c08b33eb7f2eb1
doruk-veloxia <erdemhan93@gmail.com>
2023-03-01 21:04:39 +0300

    Fix context error

--- Scripts/Utils/Helpers.cs
+++ Scripts/Utils/Helpers.cs
@@ -1,8 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using Kuantech.EndlessRunner;
 using UnityEngine;
 using UnityEngine.Events;
 
