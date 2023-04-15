96d056c722216682cbd0dd8b206e76a66b6a8133
doruk-veloxia <erdemhan93@gmail.com>
2023-04-16 00:43:43 +0300

    Added LevelElements to core library

---
+++ Scripts/HyperCasual/LevelElement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Kuantech.EndlessRunner
+{
+    public abstract class LevelElement : MonoBehaviour
+    {
+        public abstract void OnPrepareLevel();
+        public abstract void OnLeaveLevel();
+        public abstract void OnPlayLevel();
+        public abstract void OnPlayerEntered();
+        public abstract void OnPlayerExited();
+    }
+}
