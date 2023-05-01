d5343769b4d175dac9b2e58a367b4455445b302f
doruk-veloxia <erdemhan93@gmail.com>
2023-05-01 11:50:46 +0300

    Added Discrete Fill UIElement

---
+++ Scripts/HyperCasual/UI/UIElements/DiscreteFill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kuantech.Core.HyperCasual
+{
+    public class DiscreteFill : MonoBehaviour
+    {
+        [SerializeField] public List<GameObject> FillChunks;
+
+        public void SetFill(int fillAmount)
+        {
+            fillAmount = Mathf.Clamp(fillAmount, 0, FillChunks.Count);
+            for (int i = 1; i <= FillChunks.Count; ++i)
+            {
+                FillChunks[i - 1].gameObject.SetActive(i <= fillAmount);
+            }
+        }
+    }
+}
