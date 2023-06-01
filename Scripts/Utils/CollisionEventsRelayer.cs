dfce4c6e492582529d050d94a14a57c5c205d8aa
doruk-veloxia <erdemhan93@gmail.com>
2023-06-01 21:17:26 +0300

    Added event relayer

---
+++ Scripts/Utils/CollisionEventsRelayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kuantech.Core.Utils
+{
+    //A class that simply relays OnTrigger events
+    public class CollisionEventsRelayer : MonoBehaviour
+    {
+        public EventHandler<Collider> OnTriggerEnterEvent;
+        public EventHandler<Collider> OnTriggerExitEvent;
+        public EventHandler<Collision> OnCollisionEnterEvent;
+        public EventHandler<Collision> OnCollisionExitEvent;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnTriggerEnterEvent?.Invoke(this, other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            OnTriggerExitEvent?.Invoke(this,other);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            OnCollisionEnterEvent?.Invoke(this, other);
+        }
+
+        private void OnCollisionExit(Collision other)
+        {
+            OnCollisionExitEvent?.Invoke(this, other);
+        }
+    }
+}
