980c280579fff7c181652f9bf94efe6de0420d20
doruk-veloxia <erdemhan93@gmail.com>
2023-07-24 13:37:42 +0300

    Added distance culler

---
+++ Scripts/Utils/CameraDistanceCuller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Kuantech.Core.Utils
+{
+    public class CameraDistanceCuller : MonoBehaviour
+    {
+        [SerializeField] private Camera Camera;
+        [SerializeField] private float[] CullDistances = new float[32];
+        public bool enabled = true;
+        private void Awake()
+        {
+            if (!enabled) return;
+            Camera.layerCullDistances = CullDistances;
+        }
+    }
+}
