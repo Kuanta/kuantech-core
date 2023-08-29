d84fb26c5431697b41c5767f7d28aa4ce521170f
doruk-veloxia <erdemhan93@gmail.com>
2023-08-29 13:21:14 +0300

    Added ability to define a moving transform to world point struct

--- Scripts/Utils/UtilityStructs.cs
+++ Scripts/Utils/UtilityStructs.cs
@@ -7,5 +7,13 @@ namespace Kuantech.Utils
     {
         public Vector3 Position;
         public Quaternion Rotation;
+
+        public Transform Target;
+        public Vector3 LocalPosition;
+
+        public Vector3 GetTargetPosition()
+        {
+            return Target != null ? Target.TransformPoint(LocalPosition) : Position;
+        }
     }
 }
