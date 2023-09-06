77059a5c8dd578188ad55b9be003a3bf7ffbc5e5
doruk-veloxia <erdemhan93@gmail.com>
2023-09-06 13:11:31 +0300

    Added reset attribute to variables

---
+++ Scripts/Utils/Attributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kuantech.Core.Utils
+{
+    public class ResettableAttribute : Attribute
+    {
+        public object DefaultVal { get; private set; }
+
+        public ResettableAttribute(object defaultVal = null)
+        {
+            DefaultVal = defaultVal;
+        }
+    }
+}
