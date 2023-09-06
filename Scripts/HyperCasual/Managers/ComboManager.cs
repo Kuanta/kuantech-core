a200e6bfc9ddcdf35a7c2e7b4b9a6ceefc969cdd
doruk-veloxia <erdemhan93@gmail.com>
2023-09-06 16:05:44 +0300

    Added combo indicator and combo manager

---
+++ Scripts/HyperCasual/Managers/ComboManager.cs
@@ -0,0 +1,52 @@
+﻿using Cysharp.Threading.Tasks;
+using Kuantech.Core.HyperCasual.UI;
+using UnityEngine;
+
+namespace Kuantech.Core.HyperCasual
+{
+    public class ComboManager : SubManager
+    {
+        public float ComboMultiplierIncrement = 0.5f;
+        public int ComboMultiplierIncreaseThresh = 5; //Every this combo, increase combo multiplier by ComboMultiplier
+        private int _currentComboCount;
+        [SerializeField] private ComboIndicator ComboIndicator;
+
+        
+        public override async UniTask Initialize(GameManager gameManager)
+        {
+            await base.Initialize(gameManager);
+            (ParentManager as HCGameManager).StateChangeEvent += OnStateChange;
+            UIManager uiManager = ParentManager.GetSubManagerByType<UIManager>() as UIManager;
+        }
+        
+        public void IncreaseComboCount()
+        {
+            _currentComboCount++;
+            UpdateComboUI();
+        }
+
+        public float GetComboMultiplier()
+        {
+            int amount = Mathf.FloorToInt(_currentComboCount / (float)ComboMultiplierIncreaseThresh);
+            return 1 + amount * ComboMultiplierIncrement;
+        }
+        
+        public void ResetCombo()
+        {
+            _currentComboCount = 0;
+            UpdateComboUI();
+        }
+
+        private void UpdateComboUI()
+        {
+            if (ComboIndicator == null) return;
+            ComboIndicator.SetComboCounter(_currentComboCount);
+        }
+
+        private void OnStateChange(object sender, StateChangeData stateChangeData)
+        {
+            if (stateChangeData.NewState != LevelState.Waiting) return;
+            ResetCombo();
+        }
+    }
+}
---
+++ Scripts/UI/Elements/ComboIndicator.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Kuantech.Core.HyperCasual.UI
+{
+    public class ComboIndicator : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text ComboCounterText;
+        [SerializeField] private Animator Animator;
+        private static readonly int Combo = Animator.StringToHash("Combo");
+
+        public void SetComboCounter(int comboCount)
+        {
+            ComboCounterText.text = $"x {comboCount.ToString()}";;
+            if (comboCount == 0) return;
+            Animator.SetTrigger(Combo);
+        }
+    }
+}
