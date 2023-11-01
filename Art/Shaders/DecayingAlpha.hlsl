0708d3c329b5fcbf6dd651f15a97cb3079813a3a
doruk-veloxia <erdemhan93@gmail.com>
2023-11-01 16:20:38 +0300

    Added some shaders

---
+++ Art/Shaders/DecayingAlpha.hlsl
@@ -0,0 +1,31 @@
+void FadeTowardsEdges_float(float3 position, float3 pivotPoint, float distanceThreshold, float alphaDecay, out float Out)
+{
+    float3 distance = abs(position - pivotPoint);
+    float singleDistance = max(distance.x, distance.y);
+    if(singleDistance <= distanceThreshold)
+    {
+        Out = 1.0f;
+        return;
+    }
+    Out  = 1 -singleDistance*alphaDecay;
+}
+
+void FadeTowardsEdges2D_float(float2 position, float2 rectSize, out float Out)
+{
+    Out = (1-abs(position.x/rectSize.x))*(1- abs(position.y/rectSize.y));
+}
+
+void FadeTowardsEdgesCircular2D_float(float2 position, float radius, out float Out)
+{
+    //Implement circular here
+    const float2 center = 0;
+    const float distance = length(position - center);
+    const float normalizedDistance = distance / (radius); // Assuming width and height are the same for a circular shape
+    Out = 1 - normalizedDistance;
+}
+
+void FadeTowardsEdges1D_float(float position, float rectSize, out float Out)
+{
+    Out = (1- pow((position/rectSize),4));
+
+}
