c85e71ef92cb98dca568dd6e184a520295be964c
doruk-veloxia <erdemhan93@gmail.com>
2023-11-19 15:59:34 +0300

    Added random element getter for power level array

--- Scripts/Utils/PowerLevelArray.cs
+++ Scripts/Utils/PowerLevelArray.cs
@@ -16,6 +16,7 @@ namespace Kuantech.Core.Utils
         }
         public List<PowerLevelArrayElement> Elements;
         private List<T> _availableElements;
+        private List<PowerLevelArrayElement> _filteredElements;
         private int _currentPowerLevel;
         
         public PowerLevelArray()
@@ -35,18 +36,43 @@ namespace Kuantech.Core.Utils
             
             powerLevel = Mathf.Min(maxPowerLevel, powerLevel); //So that we have at least some content if power level is too high
             _availableElements = new List<T>();
+            _filteredElements = new List<PowerLevelArrayElement>();
             foreach (var element in Elements)
             {
-                if(element.MaxLevel >= powerLevel && element.MinLevel<= powerLevel) _availableElements.Add(element.Element);
+                if(element.MaxLevel >= powerLevel && element.MinLevel<= powerLevel) 
+                {
+                    _availableElements.Add(element.Element);
+                    _filteredElements.Add(element);
+                }
             }
         }
 
+        public void AddElement(T element, int minLevel, int maxLevel)
+        {
+            PowerLevelArrayElement newElement = new PowerLevelArrayElement()
+            {
+                Element = element,
+                MinLevel = minLevel,
+                MaxLevel = maxLevel,
+            };
+            Elements.Add(newElement);
+        }
         public List<T> GetAvailableElements(int powerLevel)
         {
             SetPowerLevel(powerLevel);
             return _availableElements;
         }
 
+        public T GetRandomElement(int powerLevel)
+        {
+            WeightedProbabilityArray<T> probArray = new WeightedProbabilityArray<T>();
+            SetPowerLevel(powerLevel);
+            foreach(var availableElement in _filteredElements)
+            {
+                probArray.AddElement(availableElement.Element, availableElement.MaxLevel);
+            }
+            return probArray.Sample();
+        }
         public List<T> GetAvailableElements()
         {
             return _availableElements ?? GetAvailableElements(_currentPowerLevel);
