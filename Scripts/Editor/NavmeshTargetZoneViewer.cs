c2070f507e4869249f126edee369d5a55cab590e
doruk-veloxia <erdemhan93@gmail.com>
2023-11-29 18:44:47 +0300

    (feat): Implemented navmesh target zone
    Navmesh target zone provides a polygon area for navmesh agents to get their destination from.

---
+++ Scripts/Editor/NavmeshTargetZoneViewer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using Kuantech.AI.Utils;
+
+[CustomEditor(typeof(NavmeshTargetZone))]
+public class PNavmeshTargetZoneViewer : Editor
+{
+    SerializedProperty pointsProperty;
+
+    void OnEnable()
+    {
+        pointsProperty = serializedObject.FindProperty("Points");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(pointsProperty, true); // For editing points in the Inspector
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    void OnSceneGUI()
+    {
+        NavmeshTargetZone polygon = (NavmeshTargetZone)target;
+
+        for (int i = 0; i < polygon.Points.Count; i++)
+        {
+            // Handle for each point
+            Vector3 point = polygon.transform.TransformPoint(polygon.Points[i]);
+            point.y = 0;
+            // You can change the handle type here. For example, use DotHandleCap for a dot.
+            float handleSize = HandleUtility.GetHandleSize(point) * 0.1f;
+            point = Handles.FreeMoveHandle(point, handleSize, Vector3.zero, Handles.DotHandleCap);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(polygon, "Move Point");
+                polygon.Points[i] = polygon.transform.InverseTransformPoint(point);
+            }
+        }
+    }
+}
