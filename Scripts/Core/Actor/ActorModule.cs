bf19c81e7e348c9ee7fa660111dc06503c37fad0
doruk-veloxia <erdemhan93@gmail.com>
2023-12-21 11:39:13 +0300

    fix(actors): Removed Actor requirement from actor modules
    Not all modules are in the same gameobject with the actor. Some are in the children. Requiring actor automatically adds Actor components to children

--- Scripts/Core/Actor/ActorModule.cs
+++ Scripts/Core/Actor/ActorModule.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 namespace Kuantech.Core
 {
-    [RequireComponent(typeof(Actor))]
     public abstract class ActorModule : MonoBehaviour {
         [NonSerialized] public Actor Actor;
         [NonSerialized] public bool Initialized;
