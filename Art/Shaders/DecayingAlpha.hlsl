e9706df7956b946083a3a6aa097235f036b20b78
doruk-veloxia <erdemhan93@gmail.com>
2024-01-04 11:03:37 +0300

    feat(shader): Fixed blob shadow shader having non valid alpha values

--- Art/Shaders/DecayingAlpha.hlsl
+++ Art/Shaders/DecayingAlpha.hlsl
@@ -4,10 +4,10 @@ void FadeTowardsEdges_float(float3 position, float3 pivotPoint, float distanceTh
     float singleDistance = max(distance.x, distance.y);
     if(singleDistance <= distanceThreshold)
     {
-        Out = 1.0f;
+        Out = 0.0f;
         return;
     }
-    Out  = 1 -singleDistance*alphaDecay;
+    Out  = max(1 -singleDistance*alphaDecay, 0);
 }
 
 void FadeTowardsEdges2D_float(float2 position, float2 rectSize, out float Out)
@@ -21,7 +21,7 @@ void FadeTowardsEdgesCircular2D_float(float2 position, float radius, out float O
     const float2 center = 0;
     const float distance = length(position - center);
     const float normalizedDistance = distance / (radius); // Assuming width and height are the same for a circular shape
-    Out = 1 - normalizedDistance;
+    Out = max(1 - normalizedDistance, 0);
 }
 
 void FadeTowardsEdges1D_float(float position, float rectSize, out float Out)
