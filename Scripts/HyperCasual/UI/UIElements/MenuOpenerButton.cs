b28db6387b847391f4873df1b6aa593d0a29d5b0
doruk-veloxia <erdemhan93@gmail.com>
2024-02-09 13:26:03 +0300

    feat(ui): Implemented menu opener buttonm

---
+++ Scripts/HyperCasual/UI/UIElements/MenuOpenerButton.cs
@@ -0,0 +1,16 @@
+using Kuantech.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kuantech.Core.UI
+{
+    public class MenuOpenerButton : MonoBehaviour
+    {
+        [SerializeField] private Button Button;
+        [SerializeField] private UIMenu MenuToOpen;
+        private void Start()
+        {
+            Button.onClick.AddListener(MenuToOpen.Show);
+        }
+    }
+}
