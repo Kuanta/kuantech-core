04d4f269d3ff287da08d7454937be62f859cee18
doruk-veloxia <erdemhan93@gmail.com>
2024-02-18 12:07:29 +0300

    feat(utils): Added limits to leveled values

--- Scripts/Utils/LeveledValue.cs
+++ Scripts/Utils/LeveledValue.cs
@@ -10,11 +10,20 @@ namespace Kuantech.Utils
         [SerializeField] private float BaseValue;
         [SerializeField] private float ValuePerLevel;
         [SerializeField] private int LevelIntervals;
+        [Header("Limits")]
+        [SerializeField] private int MinValue;
+        [SerializeField] private int MaxValue;
+        [SerializeField] private bool LimitValue;
 
         public float GetValue(int level = 0)
         {
             if (LevelIntervals > 0) level = Mathf.FloorToInt(level / LevelIntervals);
-            return BaseValue + (ValuePerLevel * level);
+            float value = BaseValue + (ValuePerLevel * level);
+            if (LimitValue)
+            {
+                return Mathf.Clamp(value, MinValue, MaxValue);
+            }
+            return value;
         }
     }
 
@@ -24,11 +33,20 @@ namespace Kuantech.Utils
         [SerializeField] private float BaseValue;
         [SerializeField] private float ValuePerLevel;
         [SerializeField] private int LevelIntervals = 0;
+        [Header("Limits")]
+        [SerializeField] private int MinValue;
+        [SerializeField] private int MaxValue;
+        [SerializeField] private bool LimitValue;
 
         public int GetValue(int level = 0)
         {
             if(LevelIntervals > 0) level = Mathf.FloorToInt(level / LevelIntervals);
-            return Mathf.FloorToInt(BaseValue + (ValuePerLevel * level));
+            int value = Mathf.FloorToInt(BaseValue + (ValuePerLevel * level));
+            if(LimitValue)
+            {
+                return Mathf.Clamp(value, MinValue, MaxValue);
+            }
+            return value;
         }
     }
 }
