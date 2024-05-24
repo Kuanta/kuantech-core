73507d1f660f7b7d2c4b8b459e0380b2745c9b58
doruk-veloxia <erdemhan93@gmail.com>
2024-05-24 13:33:48 +0300

    Temporary wisdom sdk

---
+++ Scripts/AnimalSorterGameManager.cs
@@ -0,0 +1,26 @@
+﻿using Kuantech.Core;
+using SupersonicWisdomSDK;
+
+namespace Kuantech.GridlessSort
+{
+    public class AnimalSorterGameManager : GameManager
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            // Subscribe
+            SupersonicWisdom.Api.AddOnReadyListener(OnSupersonicWisdomReady);
+            // Then initialize
+            SupersonicWisdom.Api.Initialize();
+        }
+
+        protected override async void Start()
+        {
+            //DO nothing
+        }
+        private void OnSupersonicWisdomReady()
+        {
+            StartGame();
+        }
+    }
+}
