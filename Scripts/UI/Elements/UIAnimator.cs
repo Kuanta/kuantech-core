3a4214c890147924c6bbf2d5afd1cda8d4dae240
doruk-veloxia <erdemhan93@gmail.com>
2024-09-05 16:59:51 +0300

    feat(ui): Added ui animator element

---
+++ Scripts/UI/Elements/UIAnimator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Kuantech.UI
+{
+    /// <summary>
+    /// A class to handle animations for ui elements. For now it supports a single element
+    /// </summary>
+    public class UIAnimator : MonoBehaviour
+    {
+        [Header("Animator")] 
+        [SerializeField] private Animator Animator;
+
+        private static readonly int Animate = Animator.StringToHash("Animate");
+        
+        //Events
+        public UnityAction OnAnimationEnd;
+        
+        // public struct AnimationClipEntry
+        // {
+        //     [KTTag("AnimationTag")]
+        //     public int AnimatonTag;
+        //     
+        //     public string TriggerName;
+        // }
+        //
+        // [Header("Animator")] 
+        // [SerializeField] private Animator Animator;
+        //
+        // public List<AnimationClipEntry> Clips;
+        // private Dictionary<int, AnimationClipEntry> _clipsMap;
+
+        public void PlayAnimation()
+        {
+            if (Animator == null) return;
+            Animator.SetTrigger(Animate);
+        }
+
+        public void Reset()
+        {
+            if (Animator == null) return;
+            Animator.Rebind();
+        }
+        
+        /// <summary>
+        /// Animation event handler
+        /// </summary>
+        public void OnAnimationEndHandler()
+        {
+            
+        }
+    }
+}
