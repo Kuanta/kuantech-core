8d2a28a04cbb0fefb16e629cb906789027981088
doruk-veloxia <erdemhan93@gmail.com>
2024-09-05 19:30:25 +0300

    feat(utils): Implemented mobile tools manager and haptic feedback

--- Scripts/Utils/Mobile/MobileToolsManager.cs
+++ Scripts/Utils/Mobile/MobileToolsManager.cs
@@ -1,7 +1,57 @@
-﻿namespace Kuantech.Utils.Mobile
+﻿using System;
+using Kuantech.Core;
+using UnityEngine;
+using CandyCoded.HapticFeedback;
+using IngameDebugConsole;
+
+namespace Kuantech.Utils.Mobile
 {
-    public class MobileToolsManager
+  
+    public class MobileToolsManager : SubManager
     {
+        #region Haptic Feedback
+        public enum HapticMagnitudes
+        {
+            Light, Medium, Heavy,
+        }
+        [Header("Vibrations")]
+        [SerializeField] private float VibrationCooldown = 0.2f;
+        private float _lastVibrationTime;
+        #endregion
         
+        public static void ApplyHaptic(HapticMagnitudes mag)
+        {
+            var context = GetContext<MobileToolsManager>();
+            if (context == null)
+            {
+                Debug.LogWarning("Add Mobile tools manager to apply haptic feedbacl");
+                return;
+            }
+            if (Time.time - context._lastVibrationTime < context.VibrationCooldown) return;
+            context._lastVibrationTime = Time.time;
+            switch (mag)
+            {
+                case HapticMagnitudes.Light:
+                    HapticFeedback.LightFeedback();
+                    break;
+                case HapticMagnitudes.Medium:
+                    HapticFeedback.MediumFeedback();
+                    break;
+                case HapticMagnitudes.Heavy:
+                    HapticFeedback.HeavyFeedback();
+                    break;
+            }
+        }
+
+        [ConsoleMethod("setHapticCooldown", "Sets haptic feedback cooldown")]
+        public static void SetHapticCooldown(float cooldown)
+        {
+            var context = GetContext<MobileToolsManager>();
+            if (context == null)
+            {
+                return;
+            }
+            context.VibrationCooldown = cooldown;
+        }
     }
 }
