05ccfeea3e8f06f6f35cf051717027ead1747bf4
doruk-veloxia <erdemhan93@gmail.com>
2024-10-24 09:53:26 +0300

    Update SheetReader.cs
    Added sheet Reader

--- Scripts/Data/SheetReader.cs
+++ Scripts/Data/SheetReader.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Util.Store;
 using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
+
 
 namespace Kuantech.Data
 {
@@ -16,6 +24,8 @@ namespace Kuantech.Data
 
         public UnityAction<JObject> OnSheetRead;
         public UnityAction OnSheetFailedToRead;
+        public UnityAction OnSheetWritten;
+        public UnityAction OnSheetFailedToWrite;
         
         public string GetRequestUrl(string SheetRange)
         {
@@ -59,14 +69,103 @@ namespace Kuantech.Data
             JObject data = JObject.Parse(json);
             OnSheetRead?.Invoke(data);
         }
-        //
-        // #region Utils
-        //
-        // public static List<float> CommaSeperatedToList(string commaSeperated)
-        // {
-        //     
-        // }
-        // #endregion
+
+        public async UniTask WriteToSheet(string SheetRange, JArray values)
+        {
+            string url = $"https://sheets.googleapis.com/v4/spreadsheets/{SheetId}/values/{SheetRange}:append?valueInputOption=USER_ENTERED&key={ApiKey}";
+            JObject body = new JObject
+            {
+                ["range"] = SheetRange,
+                ["values"] = values
+            };
+            string jsonBody = body.ToString();
+            string accessToken = await GetAccessToken();
+            try
+            {
+                using (UnityWebRequest request = new UnityWebRequest(url, "POST"))
+                {
+                    // Add the body to the request
+                    byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonBody);
+                    request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+                    request.downloadHandler = new DownloadHandlerBuffer();
+                    request.SetRequestHeader("Content-Type", "application/json");
+                    request.SetRequestHeader("Authorization", $"Bearer {accessToken}");
+                    request.timeout = 3;
+                    var operation = await request.SendWebRequest().ToUniTask();
+
+                    if (request.result != UnityWebRequest.Result.Success)
+                    {
+                        Debug.LogError($"Error writing to sheet: {request.error}");
+                        OnSheetFailedToWrite?.Invoke();
+                    }
+                    else
+                    {
+                        Debug.Log("Data successfully written to Google Sheet.");
+                        OnSheetWritten?.Invoke();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.ToString());
+                OnSheetFailedToWrite?.Invoke();
+            }
+        }
+
+        #region Auth
+        private SheetsService service;
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "Sheet Reader";
+        
+        private async Task<string> GetAccessToken()
+        {
+            // Google Cloud Console'dan indirdiğiniz JSON dosyasının yolu
+            string credentialPath = Path.Combine(Application.streamingAssetsPath, "credentials.json");
+
+            // ClientSecrets'i yükleyin
+            ClientSecrets clientSecrets = GoogleClientSecrets.FromFile(credentialPath).Secrets;
+
+            // Kullanıcıdan yetkilendirme isteyin
+            UserCredential userCredential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                clientSecrets, // ClientSecrets kullanın
+                new[] { SheetsService.Scope.Spreadsheets },
+                "user",
+                CancellationToken.None);
+
+            return userCredential.Token.AccessToken;
+        }
+        public async void AuthenticateAndInitializeService()
+        {
+            string credentialPath = Path.Combine(Application.streamingAssetsPath, "credentials.json");
+
+            using (var stream = new FileStream(credentialPath, FileMode.Open, FileAccess.Read))
+            {
+                try
+                {
+                    var credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        Scopes,
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore("TokenStore", true));
+
+                    // Google Sheets API hizmetini oluştur
+                    service = new SheetsService(new BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                        ApplicationName = ApplicationName,
+                    });
+
+                    Debug.Log("Google Sheets API authenticated and service initialized!");
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Error during Google authentication: {e.Message}");
+                }
+            }
+        }
+
+        #endregion
     }
     
 }
