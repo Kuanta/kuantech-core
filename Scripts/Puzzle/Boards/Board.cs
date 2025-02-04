e0701fda38ce0e7143b3f4518b7d29ecec2c04d1
doruk-veloxia <erdemhan93@gmail.com>
2025-02-04 18:11:04 +0300

    Added board class

---
+++ Scripts/Puzzle/Boards/Board.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kuantech.Puzzle
+{
+    public class BoardTileCoordinate
+    {
+        public BoardTileCoordinate()
+        {
+            Layer = 0;
+            Row = -1;
+            Column = -1;
+            Height = -1;
+        }
+        //Common params
+        public int Layer;
+        public int Row;
+        public int Column;
+        
+        //Voxel board params
+        public int Height;
+    }
+    
+    public abstract class Board : MonoBehaviour
+    {
+        [Header("Existing Tiles")] 
+        public List<GameObject> ExistingLayers;
+        
+        //Editor
+        [Header("Editor Background")] 
+        [SerializeField] protected GameObject Editorbackground;
+
+        public virtual void CreateBoard()
+        {
+        }
+        
+        #region Tile Operations
+        public virtual bool IsTileValidAndEmpty(BoardTileCoordinate tileCoordinate)
+        {
+            return true;
+        }
+        
+        public virtual bool CanSetTile(BoardTile tile, BoardTileCoordinate coordinate)
+        {
+            if (!IsTileValidAndEmpty(coordinate)) return false;
+            return true;
+        }
+        
+        public virtual bool SetTile(BoardTile tile, BoardTileCoordinate coordinate)
+        {
+            if (!CanSetTile(tile, coordinate)) return false;
+            if (tile.ParentBoard != null)
+            {
+                //Clear previous position
+                tile.ParentBoard.UnsetTile(tile);
+            }
+            tile.ParentBoard = this;
+            tile.CurrentCoordinate = coordinate;
+            RegisterTile(tile, coordinate);
+            return true;
+        }
+
+        public virtual void UnsetTile(BoardTile tile)
+        {
+            UnregisterTile(tile);
+            tile.ParentBoard = null;
+            tile.CurrentCoordinate = null;
+        }
+        
+        public abstract BoardTile GetTile(BoardTileCoordinate qbertTileCoordinate);
+        
+        /// <summary>
+        /// Saves the tile. Every board must have its own implementation
+        /// </summary>
+        /// <param name="tile"></param>
+        /// <param name="coordinate"></param>
+        protected abstract void RegisterTile(BoardTile tile, BoardTileCoordinate coordinate);
+        
+        /// <summary>
+        /// Clears the tile info
+        /// </summary>
+        /// <param name="tile"></param>
+        protected abstract void UnregisterTile(BoardTile tile);
+        
+        #endregion
+
+        #region Tile Coordinates
+        public abstract BoardTileCoordinate GetTileCoordinateFromWorldPosition(Vector3 worldPosition);
+        
+        public abstract Vector3 GetLocalPosition(BoardTileCoordinate tileCoordinate);
+        
+        public abstract bool IsCoordinateValid(BoardTileCoordinate tileCoordinate);
+        
+        #endregion
+    }
+}
---
+++ Scripts/Puzzle/Boards/BoardTile.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Kuantech.Puzzle
+{
+    public abstract class BoardTile : MonoBehaviour
+    {
+        public Board ParentBoard;
+        public BoardTileCoordinate CurrentCoordinate;
+        
+        public bool IsExisting;
+        public bool DestroyOnDespawn = true;
+        public bool StayOnBoardAfterDespawn = false;
+        
+        public virtual void Spawn(bool isExisting = false)
+        {
+            IsExisting = isExisting;
+        }
+        
+        public virtual void Despawn(bool clearingBoard)
+        {
+            if (!clearingBoard && StayOnBoardAfterDespawn)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
+            if (ParentBoard != null)
+            {
+                ParentBoard.UnsetTile(this);
+            }
+            if (DestroyOnDespawn)
+            {
+                Destroy(gameObject);
+            }
+            else if(!StayOnBoardAfterDespawn)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+        #region Events
+
+        public virtual void OnSetToBoard()
+        {
+            
+        }
+        
+        public virtual void OnUnsetFromBoard()
+        {
+            
+        }
+        #endregion
+    }
+}
