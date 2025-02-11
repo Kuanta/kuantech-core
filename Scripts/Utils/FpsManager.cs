084b8475a99ae181fa75973a715fa81a0cc16025
doruk-veloxia <erdemhan93@gmail.com>
2025-02-11 10:45:50 +0300

    Added fps manager

---
+++ Scripts/Utils/FpsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
+
+namespace Kuantech.Utils
+{
+    public class FpsManager : MonoBehaviour
+    {
+        [Header("Frame Settings")]
+        public int MaxRate = 9999;
+        public float TargetFrameRate = 60.0f;
+        float currentFrameTime;
+        void Awake()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = MaxRate;
+            currentFrameTime = Time.realtimeSinceStartup;
+            StartCoroutine(WaitForNextFrameRoutine());
+        }
+        
+        private IEnumerator WaitForNextFrameRoutine()
+        {
+            while(true)
+            {
+                currentFrameTime  += 1.0f / TargetFrameRate;
+                var t = Time.realtimeSinceStartup;
+                var sleepTime = currentFrameTime - t - 0.01f;
+                if(sleepTime > 0)
+                {
+                    Thread.Sleep((int)(sleepTime*1000.0f));
+                }
+            }
+        }
+    }
+}
