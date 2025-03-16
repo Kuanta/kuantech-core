67c823b4c677af7c0e311dc56aef44d3a3b9072b
Kuanta <erdemhan93@gmail.com>
2025-03-16 12:25:49 +0300

    feat(ui): Added limits to panel size adjuster

--- Scripts/Utils/UI/PanelSizer.cs
+++ Scripts/Utils/UI/PanelSizer.cs
@@ -7,6 +7,10 @@ namespace Kuantech.Utils.UI
         [Tooltip("Padding between elements")] public Vector2 Padding;
         [Tooltip("Margin between elements and edges")] public Vector2 Margin;
         [Tooltip("Size of the elements")]public Vector2 ElementSizes;
+        [Header("Limits")] public bool LimitWidth = false;
+        public Vector2 WidthLimits;
+        public bool LimitHeight;
+        public Vector2 HeightLimits;
         
         [SerializeField] private RectTransform RectTransform;
         
@@ -19,7 +23,14 @@ namespace Kuantech.Utils.UI
 
             float horizontalSize = Margin.x*2 + Mathf.Max(elementCount - 1, 0) * Padding.x + ElementSizes.x * elementCount;
             Vector2 currentSize = RectTransform.sizeDelta;
-            currentSize.x = horizontalSize;
+            if (LimitWidth)
+            {
+                currentSize.x = Mathf.Clamp(horizontalSize, WidthLimits.x, WidthLimits.y);
+            }
+            else
+            {
+                currentSize.x = horizontalSize;
+            }
             RectTransform.sizeDelta = currentSize;
         }
 
@@ -32,7 +43,15 @@ namespace Kuantech.Utils.UI
 
             float verticalSize = Margin.y*2 + Mathf.Max(elementCount - 1, 0) * Padding.y + ElementSizes.y * elementCount;
             Vector2 currentSize = RectTransform.sizeDelta;
-            currentSize.y = verticalSize;
+            if (LimitHeight)
+            {
+                currentSize.y = Mathf.Clamp(verticalSize, HeightLimits.x, HeightLimits.y);
+
+            }
+            else
+            {
+                currentSize.y = verticalSize;
+            }
             RectTransform.sizeDelta = currentSize;
         }
     }
