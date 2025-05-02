086a4fd1da54a05fdf24f42e3d795dc03991f000
doruk-veloxia <erdemhan93@gmail.com>
2025-05-02 19:16:45 +0300

    Implemented art style manager

---
+++ Scripts/Puzzle/ArtStyle/ArtStyleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Kuantech.Core;
+using Kuantech.Core.Utils;
+using Kuantech.Utils;
+
+namespace Kuantech.Puzzle
+{
+    public class ArtStyleManager : SubManager
+    {
+        public List<ColorPalette> ColorPalettes;
+
+        public static ColorPalette GetColorPalette(int index)
+        {
+            var context = ArtStyleManager.GetContext<ArtStyleManager>();
+            return context.ColorPalettes[index];
+        }
+        
+        public static ColorPalette GetRandomColorPalette()
+        {
+            var context = ArtStyleManager.GetContext<ArtStyleManager>();
+            return context.ColorPalettes.GetRandomElement();
+        }
+    }
+}
