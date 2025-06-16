4f2b6a4a55a8b6c50321ab30e44a706ac9644155
doruk-veloxia <erdemhan93@gmail.com>
2025-06-16 19:34:08 +0300

    Added astar project actor module

---
+++ Scripts/ThirdParty/AStarPathfindingProject/APPActorModule.cs
@@ -0,0 +1,13 @@
+﻿using Kuantech.Core;
+using Pathfinding;
+
+namespace Kuantech.ThirdParty.AStarPathfindingProject
+{
+    /// <summary>
+    /// This module is used to add the A* Pathfinding Project to the game.
+    /// </summary>
+    public class APPActorModule : ActorModule
+    {
+        public AIPath AIPath;
+    }
+}
