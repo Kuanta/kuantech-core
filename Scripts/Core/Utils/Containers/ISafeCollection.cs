ad9aad574c16ad3d66350ed1b6bc75bd9f6ad459
doruk-veloxia <erdemhan93@gmail.com>
2025-06-29 10:18:08 +0300

    feat(utils): Added safe collections that allow removal mid loop

---
+++ Scripts/Core/Utils/Containers/ISafeCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Kuantech.Core.Utils
+{
+    public interface ISafeCollection<T> : IEnumerable<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        void FlushRemovals();
+        void Clear();
+        int Count { get; }
+    }
+}
---
+++ Scripts/Core/Utils/Containers/SafeContainers.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+namespace Kuantech.Core.Utils
+{
+    public class SafeCollection<TCollection, T> : ISafeCollection<T>
+        where TCollection : ICollection<T>, IEnumerable<T>, new()
+    {
+        protected readonly TCollection items = new();
+        protected readonly HashSet<T> toRemove = new();
+
+        public virtual void Add(T item)
+        {
+            if (!items.Contains(item))
+                items.Add(item);
+        }
+
+        public virtual void Remove(T item)
+        {
+            if (items.Contains(item))
+                toRemove.Add(item);
+        }
+
+        public virtual void FlushRemovals()
+        {
+            foreach (var item in toRemove)
+            {
+                items.Remove(item);
+            }
+
+            toRemove.Clear();
+        }
+
+        public virtual void Clear()
+        {
+            items.Clear();
+            toRemove.Clear();
+        }
+
+        public virtual int Count => items.Count - toRemove.Count;
+
+        public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+    
+    public class SafeList<T> : SafeCollection<List<T>, T> { }
+    
+    public class SafeHashSet<T> : SafeCollection<HashSet<T>, T> { }
+
+}
