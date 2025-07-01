d122b3882b45889446002a1c16605ecdbdce1889
doruk-veloxia <erdemhan93@gmail.com>
2025-07-01 10:39:26 +0300

    feat(data): Implemented an actor metadata setter for actor blueprints

---
+++ Scripts/Core/Actor/Data/ActorMetadataSetter.cs
@@ -0,0 +1,11 @@
+﻿namespace Kuantech.Core
+{
+    public class ActorMetadataSetter : ActorBlueprintComponent
+    {
+        public int FactionId;
+        public override void OnActorCreated(Actor actor)
+        {
+            actor.FactionId = FactionId;
+        }
+    }
+}
