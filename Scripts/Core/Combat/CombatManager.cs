c35e5537f0c4e6f4d34a3c84b0a4cc3d411d65f9
doruk-veloxia <erdemhan93@gmail.com>
2025-07-10 15:15:45 +0300

    feat(system): Added combat manager
    Combat manager currently handes combat texts

---
+++ Scripts/Core/Combat/CombatManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Kuantech.Core
+{
+    public class CombatManager : SubManager
+    {
+        [Header("Damage Texts")] [SerializeField]
+        private FloatingDamageText DamageTextPrefab;
+
+        #region Damage Texts
+        public static void ShowDamageText(Vector3 position, DamageInfo damageInfo, bool friendly)
+        {
+            CombatManager ctx = GetContext<CombatManager>();
+            if (ctx == null) return;
+            FloatingDamageText damageText =
+                PoolManager.GetObjectFromPool(ctx.DamageTextPrefab.gameObject).GetComponent<FloatingDamageText>();
+            if (damageText == null) return;
+            damageText.transform.position = position;
+            damageText.Show(damageInfo, friendly);
+        }
+        #endregion
+        
+    }
+}
