1a6956fa17b4651f9d7827d7b103ee134576298f
doruk-veloxia <erdemhan93@gmail.com>
2025-07-10 15:09:44 +0300

    feat(ui): Implement damage numbers

---
+++ Scripts/Core/Combat/FloatingDamageText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using Kuantech.Utils;
+using TMPro;
+using UnityEngine;
+
+namespace Kuantech.Core
+{
+    public class FloatingDamageText : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text Text;
+        [SerializeField] private float DespawnDelay = 1;
+        [SerializeField] private Animator Animator;
+
+        [Header("Colors")] 
+        [SerializeField] private bool AdjustColors;
+        [SerializeField] private Color FriendlyColor;
+        [SerializeField] private Color EnemyColor;
+        
+        private IEnumerator _routine;
+        private static readonly int ShowHash = Animator.StringToHash("Show");
+
+        public void Show(DamageInfo damageInfo, bool isFriendly = false)
+        {
+            if (Animator != null)
+            {
+                Animator.SetTrigger(ShowHash);
+            }
+            Text.text = damageInfo.DamageAmount.Stringfy();
+
+            if (AdjustColors)
+            {
+                Text.color = isFriendly ? FriendlyColor : EnemyColor;
+            }
+            
+            if (_routine != null)
+            {
+                StopCoroutine(_routine);
+            }
+        
+            _routine = DespawnRoutine();
+            StartCoroutine(_routine);
+        }
+
+        private IEnumerator DespawnRoutine()
+        {
+            yield return new WaitForSeconds(DespawnDelay);
+            _routine = null;
+            PoolManager.PoolObject(gameObject);
+        }
+    }
+}
