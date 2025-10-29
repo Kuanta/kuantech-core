3310e7a6dff9c6f9de8586b89af96b882c125363
doruk-veloxia <erdemhan93@gmail.com>
2025-10-29 16:22:11 +0300

    feat(gameplay): Added player controller handlers

---
+++ Scripts/Core/Controller/KtInputActionAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Kuantech.Core.Controller
+{
+    [CreateAssetMenu(fileName = "KtInputActionAsset", menuName = "Kuantech/Input/KtInputActionAsset", order = 1)]
+    public class KtInputActionAsset : ScriptableObject
+    {
+        public string ActionId;
+    }
+}
---
+++ Scripts/Core/Utils/Camera/FirstPersonCameraFollower.cs
@@ -0,0 +1,29 @@
+﻿using Kuantech.Core.Controller;
+using Kuantech.Core.Utils;
+using UnityEngine;
+
+namespace Kuantech.Core.Camera
+{
+    public class FirstPersonCameraFollower : CameraFollower
+    {
+        //Runtime
+        private PlayerController _playerController;
+        private void Update()
+        {
+            if (_playerController == null || !CameraTarget) return;
+
+            // Set target look direction from _playerController
+            float yaw = _playerController.Yaw;
+            float pitch = _playerController.Pitch;
+
+            // Convert yaw/pitch to forward direction
+            Quaternion rot = Quaternion.Euler(pitch, yaw, 0f);
+            TargetDirection = rot * Vector3.forward;
+        }
+
+        public void SetController(PlayerController controller)
+        {
+            _playerController = controller;
+        }
+    }
+}
