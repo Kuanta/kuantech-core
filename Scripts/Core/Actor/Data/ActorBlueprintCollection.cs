ad2b97ee6f0f9196590864139a5f7ed47cdd7154
Kuanta <1369393+Kuanta@users.noreply.github.com>
2026-07-16 14:29:54 +0300

    feat(data): implement actor blueprint collection

---
+++ Scripts/Core/Actor/Data/ActorBlueprintCollection.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kuantech.Core
+{
+    [CreateAssetMenu(fileName ="ActorBlueprintCollection", menuName = "Kuantech/Data/ActorBlueprintCollection")]
+    public class ActorBlueprintCollection : ScriptableObject
+    {
+        public List<ActorBlueprint> ActorBlueprints;
+        private Dictionary<string, ActorBlueprint> _blueprintsByKey;
+
+        /// <summary>
+        /// Returns actor blueprint
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActorBlueprint GetActorBlueprint(string id)
+        {
+            ActorBlueprint toReturn = null;
+            if(_blueprintsByKey == null)
+            {
+                _blueprintsByKey = new Dictionary<string, ActorBlueprint>();
+                foreach(var blueprint in ActorBlueprints)
+                {
+                    string bpId = blueprint.GetId();
+                    _blueprintsByKey.Add(blueprint.GetId(), blueprint);
+                    if(bpId == id)
+                    {
+                        toReturn = blueprint;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                if(!_blueprintsByKey.ContainsKey(id)) return null;
+                toReturn = _blueprintsByKey[id];
+            }
+            return toReturn;
+        }
+    }
+}
